dfb4f59f2b79bc413b5ebbd25f801e0b84e1b2cc
Artak <jarzop@gmail.com>
2024-03-13 18:42:25 +0400

    Initial commit

---
+++ app/src/main/java/com/example/quizgame/buttons/Activities.java
@@ -0,0 +1,7 @@
+package com.example.quizgame.buttons;
+
+public enum Activities {
+    HomePage,
+    Settings,
+    Play,
+}
---
+++ app/src/main/java/com/example/quizgame/buttons/Button.java
@@ -0,0 +1,5 @@
+package com.example.quizgame.buttons;
+
+public class Button {
+
+}
